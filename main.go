0232cb0314b1ac2f90446df068e53539b4c0d8cb
Fernando <iwxfer@gmail.com>
2019-05-15 19:04:48 -0500

    db read

--- main.go
+++ main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/yanzay/tbot"
 )
+import bolt "go.etcd.io/bbolt"
 
 func main() {
-	bot := tbot.New(os.Getenv("TELEGRAM_TOKEN"))
+	bot := tbot.New("872512071:AAGqTVYoot2MJphPFtfGixmC2oo9UM9MEak")
 	c := bot.Client()
+
 	bot.HandleMessage(".*yo.*", func(m *tbot.Message) {
 		c.SendChatAction(m.Chat.ID, tbot.ActionTyping)
 		time.Sleep(1 * time.Second)
 		c.SendMessage(m.Chat.ID, "hello!")
 	})
+
+	bot.HandleMessage("read", func(m *tbot.Message) {
+		c.SendMessage(m.Chat.ID, "init")
+		db, err := bolt.Open("C:/tmp/bolt.db", 0666, nil)
+		if err != nil {
+			fmt.Sprintf(err.Error())
+		}
+
+		defer db.Close()
+
+		// TODO: read boltDB book index / bbolt
+		// TODO: read next X words  starting from index
+		// TODO: send words to bot
+		// TODO: update index
+	})
+
 	err := bot.Start()
 	if err != nil {
 		log.Fatal(err)
