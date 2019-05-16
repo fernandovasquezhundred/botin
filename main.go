387ded555d8d37939afe4cdf74b0905efd8bc176
Fernando <iwxfer@gmail.com>
2019-05-15 19:17:42 -0500

    exe

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/yanzay/tbot"
 )
 import bolt "go.etcd.io/bbolt"
+
+func Find(fname string, from, to int, needle []byte) (bool, error) {
+	f, err := os.Open(fname)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+	n := 0
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		n++
+		if n < from {
+			continue
+		}
+		if n > to {
+			break
+		}
+		if bytes.Index(scanner.Bytes(), needle) >= 0 {
+			return true, nil
+		}
+	}
+	return false, scanner.Err()
+}
 
 func main() {
 	bot := tbot.New("872512071:AAGqTVYoot2MJphPFtfGixmC2oo9UM9MEak")
@@ -25,8 +51,19 @@ func main() {
 		if err != nil {
 			fmt.Sprintf(err.Error())
 		}
-
 		defer db.Close()
+
+		db.View(func(tx *bolt.Tx) error {
+
+			b := tx.Bucket([]byte("books"))
+			v := b.Get([]byte("book1"))
+			fmt.Printf("The answer is: %s\n", v)
+
+			found, err := Find("C:\tmp\EgoIstheEnemybyRyanHoliday.txt", 18, 27, []byte("Hello World"))
+			c.SendMessage(found, "init")
+			fmt.Println(found, err)
+			return nil
+		})
 
 		// TODO: read boltDB book index / bbolt
 		// TODO: read next X words  starting from index
